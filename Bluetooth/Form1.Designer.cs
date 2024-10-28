c0414ff2114353c6b83210cb6e10280f05ce9010
szymic <szymon.julian.michalowski@gmail.com>
2024-10-28 19:59:35 +0100

    Add project files.

---
+++ Bluetooth/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Bluetooth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFindDevices = new System.Windows.Forms.Button();
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.buttonSendFile = new System.Windows.Forms.Button();
+            this.buttonPairDevices = new System.Windows.Forms.Button();
+            this.listBoxDevices = new System.Windows.Forms.ListBox();
+            this.labelNearbyDevices = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelPairedDevice = new System.Windows.Forms.Label();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.listBoxServices = new System.Windows.Forms.ListBox();
+            this.labelServices = new System.Windows.Forms.Label();
+            this.labelTransferFiles = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonFindDevices
+            // 
+            this.buttonFindDevices.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonFindDevices.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonFindDevices.Location = new System.Drawing.Point(47, 481);
+            this.buttonFindDevices.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonFindDevices.Name = "buttonFindDevices";
+            this.buttonFindDevices.Size = new System.Drawing.Size(306, 42);
+            this.buttonFindDevices.TabIndex = 0;
+            this.buttonFindDevices.Text = "Find devices";
+            this.buttonFindDevices.UseVisualStyleBackColor = false;
+            this.buttonFindDevices.Click += new System.EventHandler(this.buttonFindDevices_Click);
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonSelectFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSelectFile.Location = new System.Drawing.Point(378, 324);
+            this.buttonSelectFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(108, 35);
+            this.buttonSelectFile.TabIndex = 1;
+            this.buttonSelectFile.Text = "Select file";
+            this.buttonSelectFile.UseVisualStyleBackColor = false;
+            this.buttonSelectFile.Click += new System.EventHandler(this.buttonChooseFile_Click);
+            // 
+            // buttonSendFile
+            // 
+            this.buttonSendFile.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonSendFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSendFile.Location = new System.Drawing.Point(672, 369);
+            this.buttonSendFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSendFile.Name = "buttonSendFile";
+            this.buttonSendFile.Size = new System.Drawing.Size(108, 48);
+            this.buttonSendFile.TabIndex = 2;
+            this.buttonSendFile.Text = "Send file";
+            this.buttonSendFile.UseVisualStyleBackColor = false;
+            this.buttonSendFile.Click += new System.EventHandler(this.buttonSendFile_Click);
+            // 
+            // buttonPairDevices
+            // 
+            this.buttonPairDevices.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonPairDevices.Enabled = false;
+            this.buttonPairDevices.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonPairDevices.Location = new System.Drawing.Point(47, 527);
+            this.buttonPairDevices.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonPairDevices.Name = "buttonPairDevices";
+            this.buttonPairDevices.Size = new System.Drawing.Size(306, 42);
+            this.buttonPairDevices.TabIndex = 3;
+            this.buttonPairDevices.Text = "Pair";
+            this.buttonPairDevices.UseVisualStyleBackColor = false;
+            this.buttonPairDevices.Click += new System.EventHandler(this.buttonPairDevices_Click);
+            // 
+            // listBoxDevices
+            // 
+            this.listBoxDevices.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBoxDevices.FormattingEnabled = true;
+            this.listBoxDevices.ItemHeight = 22;
+            this.listBoxDevices.Location = new System.Drawing.Point(47, 68);
+            this.listBoxDevices.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBoxDevices.Name = "listBoxDevices";
+            this.listBoxDevices.Size = new System.Drawing.Size(306, 400);
+            this.listBoxDevices.TabIndex = 4;
+            this.listBoxDevices.SelectedIndexChanged += new System.EventHandler(this.listBoxDevices_SelectedIndexChanged);
+            // 
+            // labelNearbyDevices
+            // 
+            this.labelNearbyDevices.AutoSize = true;
+            this.labelNearbyDevices.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelNearbyDevices.Location = new System.Drawing.Point(126, 30);
+            this.labelNearbyDevices.Name = "labelNearbyDevices";
+            this.labelNearbyDevices.Size = new System.Drawing.Size(153, 25);
+            this.labelNearbyDevices.TabIndex = 9;
+            this.labelNearbyDevices.Text = "Nearby devices:";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelStatus.Location = new System.Drawing.Point(403, 144);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(66, 22);
+            this.labelStatus.TabIndex = 13;
+            this.labelStatus.Text = "Status:";
+            // 
+            // labelPairedDevice
+            // 
+            this.labelPairedDevice.AutoSize = true;
+            this.labelPairedDevice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelPairedDevice.Location = new System.Drawing.Point(475, 144);
+            this.labelPairedDevice.Name = "labelPairedDevice";
+            this.labelPairedDevice.Size = new System.Drawing.Size(123, 22);
+            this.labelPairedDevice.TabIndex = 14;
+            this.labelPairedDevice.Text = "not connected";
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxFilePath.Location = new System.Drawing.Point(492, 328);
+            this.textBoxFilePath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.ReadOnly = true;
+            this.textBoxFilePath.Size = new System.Drawing.Size(288, 28);
+            this.textBoxFilePath.TabIndex = 16;
+            // 
+            // listBoxServices
+            // 
+            this.listBoxServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBoxServices.FormattingEnabled = true;
+            this.listBoxServices.ItemHeight = 22;
+            this.listBoxServices.Location = new System.Drawing.Point(815, 68);
+            this.listBoxServices.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBoxServices.Name = "listBoxServices";
+            this.listBoxServices.Size = new System.Drawing.Size(306, 400);
+            this.listBoxServices.TabIndex = 17;
+            // 
+            // labelServices
+            // 
+            this.labelServices.AutoSize = true;
+            this.labelServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelServices.Location = new System.Drawing.Point(817, 30);
+            this.labelServices.Name = "labelServices";
+            this.labelServices.Size = new System.Drawing.Size(297, 25);
+            this.labelServices.TabIndex = 19;
+            this.labelServices.Text = "Services available on the device:";
+            // 
+            // labelTransferFiles
+            // 
+            this.labelTransferFiles.AutoSize = true;
+            this.labelTransferFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTransferFiles.Location = new System.Drawing.Point(539, 293);
+            this.labelTransferFiles.Name = "labelTransferFiles";
+            this.labelTransferFiles.Size = new System.Drawing.Size(120, 22);
+            this.labelTransferFiles.TabIndex = 20;
+            this.labelTransferFiles.Text = "Transfer files:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1165, 590);
+            this.Controls.Add(this.labelTransferFiles);
+            this.Controls.Add(this.labelServices);
+            this.Controls.Add(this.listBoxServices);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.labelPairedDevice);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelNearbyDevices);
+            this.Controls.Add(this.listBoxDevices);
+            this.Controls.Add(this.buttonPairDevices);
+            this.Controls.Add(this.buttonSendFile);
+            this.Controls.Add(this.buttonSelectFile);
+            this.Controls.Add(this.buttonFindDevices);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bluetooth";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFindDevices;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.Button buttonSendFile;
+        private System.Windows.Forms.Button buttonPairDevices;
+        private System.Windows.Forms.ListBox listBoxDevices;
+        private System.Windows.Forms.Label labelNearbyDevices;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelPairedDevice;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.ListBox listBoxServices;
+        private System.Windows.Forms.Label labelServices;
+        private System.Windows.Forms.Label labelTransferFiles;
+    }
+}
+
---
+++ Bluetooth/Form1.cs
@@ -0,0 +1,296 @@
+﻿using InTheHand.Net.Bluetooth;
+using InTheHand.Net;
+using InTheHand.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Bluetooth
+{
+    public partial class Form1 : Form
+    {
+        // Klient bluetooth
+        BluetoothClient bluetoothClient;
+
+        // Lista do przechowywania znalezionych pobliskich urzadzen
+        List<BluetoothDeviceInfo> foundDevices;
+
+        public Form1()
+        {
+            while (!IsBluetoothEnabled())
+            {
+                // Otwarcie ustawień Bluetooth systemu Windows
+                System.Diagnostics.Process.Start("ms-settings:bluetooth");
+                MessageBox.Show("Please enable Bluetooth on your device and then press OK.", "Bluetooth disabled on the device", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            InitializeComponent();
+            bluetoothClient = new BluetoothClient();
+            foundDevices = new List<BluetoothDeviceInfo>();
+            
+        }
+
+        public bool IsBluetoothEnabled()
+        {
+            try
+            {
+                // Sprzwdzenie, czy urządzenie (komputer) posiada moduł bluetooth
+                BluetoothRadio primaryRadio = BluetoothRadio.PrimaryRadio;
+
+                if (primaryRadio == null) return false;
+                if (primaryRadio.Mode == RadioMode.PowerOff) return false;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error checking Bluetooth status: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private static Dictionary<Guid, string> BluetoothServiceDictionary = new Dictionary<Guid, string>
+        {
+            // Communication Services
+            { Guid.Parse("00001101-0000-1000-8000-00805f9b34fb"), "Serial Port Service" },
+            { Guid.Parse("00001102-0000-1000-8000-00805f9b34fb"), "LAN Access Using PPP" },
+            { Guid.Parse("00001103-0000-1000-8000-00805f9b34fb"), "Dialup Networking" },
+    
+            // Audio and Video Services
+            { Guid.Parse("0000110A-0000-1000-8000-00805f9b34fb"), "Audio Source" },
+            { Guid.Parse("0000110B-0000-1000-8000-00805f9b34fb"), "Audio Sink" },
+            { Guid.Parse("0000110D-0000-1000-8000-00805f9b34fb"), "Advanced Audio Distribution Profile (A2DP)" },
+            { Guid.Parse("0000110E-0000-1000-8000-00805f9b34fb"), "Hands-Free" },
+            { Guid.Parse("0000110F-0000-1000-8000-00805f9b34fb"), "Headset - Audio Gateway (HSP)" },
+            { Guid.Parse("0000111E-0000-1000-8000-00805f9b34fb"), "Handsfree Audio Gateway" },
+            { Guid.Parse("0000111F-0000-1000-8000-00805f9b34fb"), "Phonebook Access - PCE (Client)" },
+            { Guid.Parse("00001120-0000-1000-8000-00805f9b34fb"), "Phonebook Access - PSE (Server)" },
+    
+            // OBEX Services
+            { Guid.Parse("00001105-0000-1000-8000-00805f9b34fb"), "OBEX Object Push" },
+            { Guid.Parse("00001106-0000-1000-8000-00805f9b34fb"), "OBEX File Transfer" },
+            { Guid.Parse("00001104-0000-1000-8000-00805f9b34fb"), "OBEX Synchronization Service" },
+            { Guid.Parse("00001112-0000-1000-8000-00805f9b34fb"), "OBEX IrMC Synchronization Service" },
+
+            // Remote Control and Gaming
+            { Guid.Parse("0000110C-0000-1000-8000-00805f9b34fb"), "Remote Control Target" },
+            { Guid.Parse("0000111B-0000-1000-8000-00805f9b34fb"), "SIM Access" },
+            { Guid.Parse("00001124-0000-1000-8000-00805f9b34fb"), "Human Interface Device (HID) - Keyboard/Mouse/Gamepad" },
+            { Guid.Parse("00001200-0000-1000-8000-00805f9b34fb"), "PnP Information" },
+    
+            // Health and Fitness Services
+            { Guid.Parse("0000180D-0000-1000-8000-00805f9b34fb"), "Heart Rate Service" },
+            { Guid.Parse("0000180F-0000-1000-8000-00805f9b34fb"), "Battery Service" },
+            { Guid.Parse("00001810-0000-1000-8000-00805f9b34fb"), "Blood Pressure" },
+            { Guid.Parse("00001812-0000-1000-8000-00805f9b34fb"), "Human Interface Device (HID) Service" },
+
+            // Device Information and Management
+            { Guid.Parse("0000180A-0000-1000-8000-00805f9b34fb"), "Device Information Service" },
+            { Guid.Parse("0000112F-0000-1000-8000-00805f9b34fb"), "Basic Imaging Profile (BIP)" },
+            { Guid.Parse("00001130-0000-1000-8000-00805f9b34fb"), "Basic Printing Profile (BPP)" },
+    
+            // Miscellaneous Services
+            { Guid.Parse("00001802-0000-1000-8000-00805f9b34fb"), "Immediate Alert" },
+            { Guid.Parse("00001803-0000-1000-8000-00805f9b34fb"), "Link Loss" },
+            { Guid.Parse("00001804-0000-1000-8000-00805f9b34fb"), "Tx Power" },
+            { Guid.Parse("00001805-0000-1000-8000-00805f9b34fb"), "Current Time Service" }
+        };
+
+
+        private void buttonFindDevices_Click(object sender, EventArgs e)
+        {
+            foundDevices.Clear();
+            listBoxDevices.Items.Clear();
+
+            // Szukanie pobliskich urzadzen
+            BluetoothDeviceInfo[] devices = bluetoothClient.DiscoverDevices();
+
+            foreach (BluetoothDeviceInfo device in devices)
+            {
+                foundDevices.Add(device);
+                listBoxDevices.Items.Add(device.DeviceName);
+            }
+
+            buttonPairDevices.Enabled = false;
+        }
+
+
+        private void findServices(BluetoothDeviceInfo device)
+        {
+            listBoxServices.Items.Clear();
+
+            // Pobranie dostępnych usług dla wybranego urządzenia
+            var services = device.InstalledServices;
+
+            if (services.Length == 0) // Jeśli nie ma dostępnych usług
+            {
+                MessageBox.Show("No services available on the device.", "No services", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            foreach (var service in services)
+            {
+                string translatedService;
+
+                // Jesli usluga wystepuje w slowniku - pobieramy jej nazwe
+                if (BluetoothServiceDictionary.TryGetValue(service, out translatedService))
+                {
+                    listBoxServices.Items.Add(translatedService);
+                }
+                else
+                {
+                    listBoxServices.Items.Add("Unknown service");
+                }
+            }
+        }
+
+
+        private void buttonPairDevices_Click(object sender, EventArgs e)
+        {
+            if(listBoxDevices.SelectedItem == null)
+            {
+                MessageBox.Show("Select device to pair.", "Device not selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // Pobieramy wybrane urządzenie z foundDevices
+                BluetoothDeviceInfo selectedDevice = foundDevices[listBoxDevices.SelectedIndex];
+
+                // Sprawdzamy, czy urządzenia są już sparowane
+                if (!selectedDevice.Authenticated)
+                {
+                    bool paired = BluetoothSecurity.PairRequest(selectedDevice.DeviceAddress, "0000");
+                    if(paired)
+                    {
+                        MessageBox.Show("Devices paired successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to pair devices.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Devices are already paired.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                labelPairedDevice.Text = "Paired with: " + selectedDevice.DeviceName;
+
+                // Wyszukanie i wyświetlenie zainstalowanych serwisów bluetooth na sparowanym urządzeniu
+                findServices(selectedDevice); 
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error occured while trying to pair devices: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void buttonChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Wybierz plik";
+            //openFileDialog.Filter = "Wszystkie pliki (*.*)|*.*";
+            openFileDialog.Filter = "Obrazy(*.jpg; *.jpeg; *.png)| *.jpg; *.jpeg; *.png";
+
+            // Sprawdzamy, czy użytkownik wybrał plik i zatwierdził wybór
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Wyświetlamy ścieżkę wybranego pliku w textBoxie
+                textBoxFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+
+        private bool IsObexObjectPushSupported(BluetoothDeviceInfo device)
+        {
+            // Sprawdzenie, czy urządzenie posiada usługę OBEX Object Push
+            foreach (Guid service in device.InstalledServices)
+            {
+                if (service == BluetoothService.ObexObjectPush)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private void buttonSendFile_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxFilePath.Text))
+            {
+                MessageBox.Show("Select file to send!", "File not selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Sprawdzamy, czy wybrano urządzenie z listy
+            if (listBoxDevices.SelectedItem == null)
+            {
+                MessageBox.Show("Select destination device to send file to!", "Desination device not selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // Pobieramy wybrane urządzenie z listy
+                BluetoothDeviceInfo selectedDevice = foundDevices[listBoxDevices.SelectedIndex];
+
+                // Sprawdzamy, czy urządzenie docelowe posiada usługę OBEX Object Push
+                if (!IsObexObjectPushSupported(selectedDevice))
+                {
+                    MessageBox.Show("Selected device does not support OBEX Object Push. File transfer cannot be performed.", "Unsupported Service", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Żądanie OBEX do wysłania pliku
+                Uri uri = new Uri("obex://" + selectedDevice.DeviceAddress + "/" + System.IO.Path.GetFileName(textBoxFilePath.Text));
+                ObexWebRequest request = new ObexWebRequest(uri);
+                request.ReadFile(textBoxFilePath.Text);
+
+                // Wysłanie pliku
+                /*ObexWebResponse response = (ObexWebResponse)request.GetResponse();
+                MessageBox.Show("File sent successfully. Response status: " + response.StatusCode, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                response.Close();*/
+                using (ObexWebResponse response = (ObexWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode == ObexStatusCode.OK)
+                    {
+                        MessageBox.Show("File sent successfully. Response status: " + response.StatusCode, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("File not sent. Response status: " + response.StatusCode, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occured while sending file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void listBoxDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBoxDevices.SelectedIndex == -1)
+            {
+                buttonPairDevices.Enabled = false;
+            }
+            else
+            {
+                buttonPairDevices.Enabled = true;
+            }
+        }
+    }
+}
